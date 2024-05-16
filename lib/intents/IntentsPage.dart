c00414f008ccbb96f07efca4e44f1f735bb9b3c2
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2024-05-16 17:38:03 -0400

    mas intents
    Signed-off-by: figonzal1 <felipe.gonzalezalarcon94@gmail.com>

--- lib/intents/IntentsPage.dart
+++ lib/intents/IntentsPage.dart
@@ -28,6 +28,13 @@ class _IntentsPageState extends State<IntentsPage> {
     );
   }
 
+  Future<void> explicitGMaps() async {
+    await LaunchApp.openApp(
+      androidPackageName: 'com.google.android.apps.maps',
+      openStore: true,
+    );
+  }
+
   void sendToEmail() {
     const AndroidIntent intent = AndroidIntent(
         action: 'android.intent.action.SENDTO',
@@ -36,12 +43,28 @@ class _IntentsPageState extends State<IntentsPage> {
     intent.launch();
   }
 
-  void sendToEmailWithAttach() {
+  void playVideoMedia() {
     const AndroidIntent intent = AndroidIntent(
-        action: 'android.intent.action.SEND',
-        data:
-            'mailto:correo_destino@example.com?subject=Asunto del email con adjuntos&body=Probando el envío de datos desde flutter',
-        type: "images/*");
+      action: 'android.intent.action.VIEW',
+      data:
+          'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    );
+    intent.launch();
+  }
+
+  void geoRefMaps() {
+    const AndroidIntent intent = AndroidIntent(
+      action: 'android.intent.action.VIEW',
+      data: 'geo:47.6,-122.3',
+    );
+    intent.launch();
+  }
+
+  void openUrl() {
+    const AndroidIntent intent = AndroidIntent(
+      action: 'android.intent.action.VIEW',
+      data: 'https://www.rycconsultores.cl',
+    );
     intent.launch();
   }
 
@@ -68,14 +91,14 @@ class _IntentsPageState extends State<IntentsPage> {
           children: [
             intentExternalAppLauncher(context, btnText),
             intentCalls(context, btnText),
-            intentEmails(context, btnText),
+            intentImplicitos(context, btnText),
           ],
         ),
       ),
     );
   }
 
-  Column intentEmails(BuildContext context, String btnText) {
+  Column intentImplicitos(BuildContext context, String btnText) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisSize: MainAxisSize.max,
@@ -84,12 +107,12 @@ class _IntentsPageState extends State<IntentsPage> {
           height: 20,
         ),
         const Text(
-          "Intents de email",
+          "Intents implícitos",
           style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
         ),
         ListTile(
           dense: true,
-          title: const Text("Intent implícito email"),
+          title: const Text("Intent email"),
           trailing: MaterialButton(
               color: Theme.of(context).colorScheme.inversePrimary,
               onPressed: () {
@@ -99,11 +122,31 @@ class _IntentsPageState extends State<IntentsPage> {
         ),
         ListTile(
           dense: true,
-          title: const Text("Intent implícito email"),
+          title: const Text("Intent video media"),
           trailing: MaterialButton(
               color: Theme.of(context).colorScheme.inversePrimary,
               onPressed: () {
-                sendToEmail();
+                playVideoMedia();
+              },
+              child: Text(btnText)),
+        ),
+        ListTile(
+          dense: true,
+          title: const Text("Intent geo coords (maps)"),
+          trailing: MaterialButton(
+              color: Theme.of(context).colorScheme.inversePrimary,
+              onPressed: () {
+                geoRefMaps();
+              },
+              child: Text(btnText)),
+        ),
+        ListTile(
+          dense: true,
+          title: const Text("Intent web"),
+          trailing: MaterialButton(
+              color: Theme.of(context).colorScheme.inversePrimary,
+              onPressed: () {
+                openUrl();
               },
               child: Text(btnText)),
         ),
@@ -143,6 +186,16 @@ class _IntentsPageState extends State<IntentsPage> {
               color: Theme.of(context).colorScheme.inversePrimary,
               onPressed: () {
                 explicitGmail();
+              },
+              child: Text(btnText)),
+        ),
+        ListTile(
+          dense: true,
+          title: const Text("Intent explícito GMaps"),
+          trailing: MaterialButton(
+              color: Theme.of(context).colorScheme.inversePrimary,
+              onPressed: () {
+                explicitGMaps();
               },
               child: Text(btnText)),
         ),
